dc1a07634b2d5ebfef5268f80cc6b03dbb4dd0cc
Menos  Velasco jr <manuelvelasco2020@gmail.com>
2019-11-15 10:52:47 -0800

    finish function.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -10,40 +10,49 @@ function consume(a, b, cb) {
   return cb(a, b);
 }
 
-console.log(consume());
-
 /* Step 2: Create several functions to callback with consume();
  * Create a function named add that returns the sum of two numbers
  * Create a function named multiply that returns the product of two numbers
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
 
-// function add(num, numTwo{
-//   return num * numTwo;
-// })
+function add(num, numTwo) {
+  return num + numTwo;
+}
 
+function multiply(num, numTwo) {
+  return num * numTwo;
+}
+function greeting(first, last) {
+  return `Hello ${first}, ${last}, nice to meet you!`;
+}
+
+// console.log(add(10, 2));
+// console.log(multiply(2, 3));
+// console.log(greeting(`Hello ${first}, ${last}, nice to meet you!`));
 // function multiply(num1)
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume('Mary', 'Poppins', greeting)); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ====
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
+// variables can be access in the local or global scope, but is can only be access variable from bottom to top.
+
 // Explanation:
 
-// const external = "I'm outside the function";
+const external = "I'm outside the function";
 
-// function myFunction() {
-//   console.log(external);
-//   const internal = "Hello! I'm inside myFunction!";
+function myFunction() {
+  console.log(external);
+  const internal = "Hello! I'm inside myFunction!";
 
-//   function nestedFunction() {
-//     console.log(internal);
-//   };
-//   nestedFunction();
-// };
-// myFunction();
-//
+  function nestedFunction() {
+    console.log(internal);
+  }
+  nestedFunction();
+}
+myFunction();
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -120,12 +120,10 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-for (let i = 0; i < graduates.length; i++) {
-  universities.push(graduates[i].universities);
+const universities = graduates.map(function()) {
+  return universities;
 }
-let inventorySorted = universities.sort();
-console.log(universities);
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -135,7 +133,7 @@ The resulting contact information strings should have a space between the first 
 Log the result of your new array. */
 const contactInfo = [];
 
-console.log(contactInfo;
+console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
